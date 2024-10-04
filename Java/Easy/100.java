4c4f7ee6241060a5aa894428070850a3bae278d2
Ariyan-25 <167085284+Ariyan-25@users.noreply.github.com>
2024-10-04 23:12:35 +0530

    ariyan

--- Java/Easy/100.java
+++ Java/Easy/100.java
@@ -1,6 +1,6 @@
 class Main {
     public static void main(String[] args) {
         String[] names = {"Alice", "Bob"};
-        System.out.println(names.get(0));
+        System.out.println(names[0]); 
     }
 }

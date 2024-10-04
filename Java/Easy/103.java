f7ee43ae7517c2c2b6eaf67f503256c59a25f324
Ariyan-25 <167085284+Ariyan-25@users.noreply.github.com>
2024-10-04 23:35:54 +0530

    ariyan4

--- Java/Easy/103.java
+++ Java/Easy/103.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Main {
     public static void main(String[] args) {
-        List<int> numbers = new ArrayList<>();
+        List<Integer> numbers = new ArrayList<>(); 
         numbers.add(10);
         System.out.println(numbers.get(0));
     }

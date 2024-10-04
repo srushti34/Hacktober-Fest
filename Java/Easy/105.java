c3306a96b85c0865020754eea5baeeeb693295a9
Ariyan-25 <167085284+Ariyan-25@users.noreply.github.com>
2024-10-04 23:23:03 +0530

    ariyan3

--- Java/Easy/105.java
+++ Java/Easy/105.java
@@ -1,6 +1,6 @@
 class Main {
     public static void main(String[] args) {
         String text = "java";
-        System.out.println(text.ToUpperCase());
+        System.out.println(text.toUpperCase()); 
     }
 }

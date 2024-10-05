b602c9dd87f5d56dc0f4a9fb4f2797accbe48b5f
srushti34 <srushty3404@gmail.com>
2024-10-05 12:14:59 +0530

    Update 3.py

--- Python/Easy/3.py
+++ Python/Easy/3.py
@@ -1,10 +1,19 @@
-def greet()
-print("Hello, world!")
+def greet():
+    a="Hello"
+    b="world!"
+    print ("Hello, world!")
 
-def add(a, b)
-return a + b
 
-class Car
-def __init__(self, make, model):
-self.make = make
-self.model = model
+
+def add(a, b):
+    return a + b
+
+
+class Car:
+    def __init__(self, make, model):
+        self.make = make
+        self.model = model
+
+
+
+

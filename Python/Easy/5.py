798f6baea33c90c568a2eb7a57a2a02dfa1b6636
srushti34 <srushty3404@gmail.com>
2024-10-05 12:26:23 +0530

    Update 5.py

--- Python/Easy/5.py
+++ Python/Easy/5.py
@@ -1,24 +1,24 @@
-prnt("Hello, world!")
-for i in rang(5):
+print("Hello, world!")
+for i in range(5):
     print(i)
 
 
-pritn("Hello, world!")
-for i in rnage(10):
+print("Hello, world!")
+for i in range(10):
     print(i)
 my_varaible = 10
-print(my_variable)
+print(my_varaible)
 
 x = 10
 y = 5
+z= 4
 result = x - y + z 
-if x = 5: 
+if x == 5: 
     print("x is 5")
 
 if x > 10:
     print("x is greater")
-else if x == 10:
+elif x == 10:
     print("x is equal")
 
-del my_function() 
-
+def my_function()

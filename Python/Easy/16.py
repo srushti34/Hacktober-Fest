136b9fc12fcc4c11204a429f9c9729eb4e60f92b
srushti34 <srushty3404@gmail.com>
2024-10-05 13:41:29 +0530

    Update 16.py

--- Python/Easy/16.py
+++ Python/Easy/16.py
@@ -1,5 +1,6 @@
-L=(1,2,3,4)
-L.append(5)
-my_tuple = (1, 2, 3)
-
-my_tuple.append(4)  # 'tuple' object has no 'append' method
+my_tupleL=(1,2,3,4)
+my_tupleL.insert(5)
+my_tuple =(1, 2, 3)
+my_tuple.insert(4)  # 'tuple' object has no 'append' method
+print(my_tupleL)
+print(my_tuple)

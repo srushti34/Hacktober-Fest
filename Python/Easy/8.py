8a3c2d0c685b0dc1ef925af46e81400724d752d9
srushti34 <srushty3404@gmail.com>
2024-10-05 12:44:27 +0530

    Update 8.py

--- Python/Easy/8.py
+++ Python/Easy/8.py
@@ -1,9 +1,10 @@
-if x = 5:
-    print("x is 5")
+def x():
+    if x == 5:
+        print("x is 5")
 if x == 5:
     x = 10
     print("x is now 10")
 
 x = 5
-if x = 5:
+if x == 5:
     print("x is 5")

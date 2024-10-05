a5ee57a1ac7c0475b6b0c3552b0dfe71e676fd92
srushti34 <srushty3404@gmail.com>
2024-10-05 13:26:12 +0530

    Update 15.py

--- Python/Easy/15.py
+++ Python/Easy/15.py
@@ -1,9 +1,9 @@
-Import
-print(random.randint(1,10)
+import random
+print(random.randint(1,10))
 
-Import random  # Error: Incorrect case, 'import' should be lowercase
+import random  # Error: Incorrect case, 'import' should be lowercase
 print(random.randint(1, 10))
 
-import  # Incomplete import statement
+import math  # Incomplete import statement
 print(math.sqrt(25))
 

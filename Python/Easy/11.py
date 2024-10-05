2e524925d790fd801db673d23a5f7ae00876bc31
srushti34 <srushty3404@gmail.com>
2024-10-05 13:10:35 +0530

    Update 11.py

--- Python/Easy/11.py
+++ Python/Easy/11.py
@@ -1,9 +1,9 @@
 def outer():
-    x = 10
-    def inner():
-        global x  # Error: x is local to outer and cannot be made global
-        x = 5
-    inner()
-    print(x)
-
+     x == 10
+def x():
+         global x  # Error: x is local to outer and cannot be made global
+         x=5
+         x()
+        
 outer()
+print(x)
